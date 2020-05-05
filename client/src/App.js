e287a37b66a120d5bccac4ff1a1de8e562112ef3
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-05 13:39:43 -0700

    added bolier plate for react app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import User from "./components/User";
+
+function App() {
+  return (
+    <div>
+      <h2>hello</h2>
+      <User />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/User.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const User = () => {
+  return (
+    <div className="User">
+      <h1>user</h1>
+    </div>
+  );
+};
+
+export default User;
